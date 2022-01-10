4fb5b71ff4938d407f431bffd11484bca3e66f52
Dipanshu <dipanshu.vaibhav@science.christuniversity.in>
2022-01-10 23:57:08 +0530

    SecretScreen

--- app/src/main/java/com/labyrinth/loopy/SplashScreen.java
+++ app/src/main/java/com/labyrinth/loopy/SplashScreen.java
@@ -2,20 +2,27 @@ package com.labyrinth.loopy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashScreen extends AppCompatActivity {
 
-    ImageView logo, appnameimage;
+    private static int SPLASH_SCREEN_TIME_OUT=7000;
+
+    ImageView logo;
     LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
 
         logo = findViewById(R.id.labyrinth_logo);
@@ -24,7 +31,19 @@ public class SplashScreen extends AppCompatActivity {
         logo.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreen.this, SecretScreen.class);
+                //Intent is used to switch from one activity to another.
 
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
 
     }
 }
