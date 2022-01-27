c65fd1fa5a04a603471b4534d9939144cf928389
Dipanshu <dipanshu.vaibhav@science.christuniversity.in>
2022-01-27 22:13:44 +0530

    UI Skeleton Phase 1

---
+++ app/src/main/java/com/labyrinth/loopy/StudentInfo_InputScreen.java
@@ -0,0 +1,14 @@
+package com.labyrinth.loopy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentInfo_InputScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_info_input_screen);
+    }
+}
