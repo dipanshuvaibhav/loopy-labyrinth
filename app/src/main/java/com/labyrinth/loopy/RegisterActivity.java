c869363cada060699b5addaa719863ba5e87068f
Dipanshu <dipanshu.vaibhav@science.christuniversity.in>
2022-01-28 19:44:28 +0530

    UI Skeleton Phase 2

--- app/src/main/java/com/labyrinth/loopy/RegisterActivity.java
+++ app/src/main/java/com/labyrinth/loopy/RegisterActivity.java
@@ -8,15 +8,39 @@ import android.os.Build;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText fullName, email, phone, password, confirm_password, username;
+    ImageButton register_btn, google_signup_btn, back_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         changeStatusBarColor();
 
+        fullName = (EditText) findViewById(R.id.full_name);
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.phone);
+        password = (EditText) findViewById(R.id.password);
+        confirm_password = (EditText) findViewById(R.id.confirm_password);
+        username = (EditText) findViewById(R.id.username);
+        register_btn = (ImageButton) findViewById(R.id.register_btn);
+        google_signup_btn = (ImageButton) findViewById(R.id.google_signup_btn);
+        back_btn = (ImageButton) findViewById(R.id.back_btn);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void changeStatusBarColor() {
@@ -24,7 +48,7 @@ public class RegisterActivity extends AppCompatActivity {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 //            window.setStatusBarColor(Color.TRANSPARENT);
-            window.setStatusBarColor(getResources().getColor(R.color.register_bk_color));
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
         }
     }
 
