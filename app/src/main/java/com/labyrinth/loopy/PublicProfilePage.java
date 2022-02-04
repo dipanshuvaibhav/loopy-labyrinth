3a40d11e055d099df32dd223446233da856e6225
Dipanshu <dipanshu.vaibhav@science.christuniversity.in>
2022-02-04 13:32:13 +0530

    UI Skeleton Phase 4

---
+++ app/src/main/java/com/labyrinth/loopy/PublicProfilePage.java
@@ -0,0 +1,14 @@
+package com.labyrinth.loopy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PublicProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_profile_page);
+    }
+}
