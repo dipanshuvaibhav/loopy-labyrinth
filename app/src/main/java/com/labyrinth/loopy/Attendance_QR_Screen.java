dbf121cb1cd1b5371aafd2450ae97f3b3b17ffdf
Dipanshu Vaibhav <dipanshu.vaibhav@science.christuniversity.in>
2022-02-12 18:59:02 +0530

    Skeleton UI Phase 6

---
+++ app/src/main/java/com/labyrinth/loopy/Attendance_QR_Screen.java
@@ -0,0 +1,14 @@
+package com.labyrinth.loopy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Attendance_QR_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_qr_screen);
+    }
+}
